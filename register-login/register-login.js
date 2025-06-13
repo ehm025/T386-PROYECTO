fd2e03f1fb30d2fa2b4bb213b1243a4b0ff158d7
Allan <117397343+Allitan@users.noreply.github.com>
2025-06-13 00:39:44 -0600

    Update

--- register-login/register-login.js
+++ register-login/register-login.js
@@ -9,26 +9,26 @@ const db = require('../config/db');
 
 //Registro de usuario
 router.post('/registro', async (req, res) => {
-    const { Nombre, Email, Contraseña, rol} = req.body;
+    const { nombre, email, contraseña, rol} = req.body;
 
-    if(!Nombre || !Email || !Contraseña){
+    if(!nombre || !email || !contraseña){
         return res.status(400).json({ error: 'Por favor, proporciona un nombre, email y contraseña.' });
     }
 
     try{
         // Chequeamos que el email exista
-        const emailCheckQuery = 'SELECT ID_Usuario FROM Usuarios WHERE Email = ?';
-        const [existingUser] = await db.promise().query(emailCheckQuery, [Email]);
+        const emailCheckQuery = 'SELECT id FROM usuarios WHERE email = ?';
+        const [existingUser] = await db.promise().query(emailCheckQuery, [email]);
 
         if(existingUser.length > 0){
             return res.status(400).json({ error: 'El email ya está registrado.' });
         }
 
         const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(Contraseña, saltRounds);
+        const hashedPassword = await bcrypt.hash(contraseña, saltRounds);
 
-        const insertQuery = 'INSERT INTO Usuarios (Nombre, Email, Contraseña, Rol) VALUES (?, ?, ?, ?)';
-        const [result] = await db.promise().query(insertQuery, [Nombre, Email, hashedPassword, rol,]);
+        const insertQuery = 'INSERT INTO usuarios (nombre, email, contraseña, rol) VALUES (?, ?, ?, ?)';
+        const [result] = await db.promise().query(insertQuery, [nombre, email, hashedPassword, rol,]);
 
         res.status(201).json({message: 'Usuario registrado exitosamente.', userId: result.insertId});
 
@@ -41,15 +41,15 @@ router.post('/registro', async (req, res) => {
 
 //Login de usuario
 router.post('/login', async (req, res) => {
-    const { Email, Contraseña } = req.body;
+    const { email, contraseña } = req.body;
 
-    if(!Email || !Contraseña){
+    if(!email || !contraseña){
         return res.status(400).json({ error: 'Por favor, proporciona email y contraseña.' });
     }
 
     try{
-        const query = 'SELECT ID_Usuario, Contraseña FROM Usuarios WHERE Email = ?';
-        const [results] = await db.promise().query(query, [Email]);
+        const query = 'SELECT id, contraseña FROM usuarios WHERE email = ?';
+        const [results] = await db.promise().query(query, [email]);
 
         if(results.length === 0){
             return res.status(401).json({ error: 'Credenciales inválidas.' });
@@ -57,16 +57,16 @@ router.post('/login', async (req, res) => {
 
         const user = results[0];
 
-        const passwordMatch = await bcrypt.compare(Contraseña, user.Contraseña);
+        const passwordMatch = await bcrypt.compare(contraseña, user.contraseña);
 
         if(!passwordMatch){
             return res.status(401).json({ error: 'Credenciales inválidas.' });
         }
 
         //Generamos el token
-        const token = jwt.sign({ userId: user.ID_Usuario }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user.id}, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.status(200).json({ message: 'Inicio de sesión exitoso.', token: token, userId: user.ID_Usuario });
+        res.status(200).json({ message: 'Inicio de sesión exitoso.', token: token, userId: user.id });
     } catch (error) {
         console.error('Error al iniciar sesión:', error);
         return res.status(500).json({ error: 'Error interno del servidor al iniciar sesión.' });
